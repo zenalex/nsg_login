c96a2c89cb7efb1f8862ed14916c87908c2af522
AndyProtech <andy.protech@gmail.com>
2024-12-20 15:21:26 +0300

    fix

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter_sdk\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Projects2\flutter_get\nsg_login"
+export "FLUTTER_ROOT=C:\ProgramData\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\work\zenkov\nsg_login"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
