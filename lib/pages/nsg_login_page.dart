4d994646149879e2f8258cf68a8922fe28b42879
AndyProtech <andy.protech@gmail.com>
2024-12-20 14:56:40 +0300

    nsgloginpage code from sms

--- lib/pages/nsg_login_page.dart
+++ lib/pages/nsg_login_page.dart
@@ -31,7 +31,9 @@ class NsgLoginPage extends StatelessWidget {
     return Scaffold(
       appBar: (widgetParams().appbar ?? false) ? getAppBar(context) : null,
       //backgroundColor: Colors.white,
-      body: Container(decoration: BoxDecoration(color: nsgtheme.colorMain.withOpacity(0.1)), child: LoginWidget(this, provider, widgetParams: widgetParams())),
+      body: Container(
+          decoration: BoxDecoration(color: nsgtheme.colorMain.withOpacity(0.1)),
+          child: LoginWidget(this, provider, widgetParams: widgetParams())),
     );
   }
 
@@ -127,7 +129,8 @@ class LoginWidgetState extends State<LoginWidget> {
   @override
   void initState() {
     super.initState();
-    widget.loginPage.callback.sendDataPressed = () => doSmsRequest(Get.context!, loginType: loginType, password: password, firebaseToken: firebaseToken);
+    widget.loginPage.callback.sendDataPressed =
+        () => doSmsRequest(Get.context!, loginType: loginType, password: password, firebaseToken: firebaseToken);
     if (widget.widgetParams!.usePhoneLogin) {
       loginType = NsgLoginType.phone;
     } else {
@@ -200,7 +203,8 @@ class LoginWidgetState extends State<LoginWidget> {
           alignment: Alignment.topRight,
           children: [
             Container(
-              decoration: BoxDecoration(color: nsgtheme.colorMainBack, borderRadius: const BorderRadius.all(Radius.circular(3.0))),
+              decoration: BoxDecoration(
+                  color: nsgtheme.colorMainBack, borderRadius: const BorderRadius.all(Radius.circular(3.0))),
               padding: const EdgeInsets.all(15.0),
               width: widget.widgetParams!.cardSize,
               child: Row(
@@ -311,7 +315,8 @@ class LoginWidgetState extends State<LoginWidget> {
     }
 
     //0 - успешно, 40201 - смс отправлено ранее. И в том и другом случае, переходим на экран ввода кода подтверждения
-    if ((answerCode.errorCode == 0 || answerCode.errorCode == 40201) && (currentState == NsgLoginState.registration || currentState == NsgLoginState.login)) {
+    if ((answerCode.errorCode == 0 || answerCode.errorCode == 40201) &&
+        (currentState == NsgLoginState.registration || currentState == NsgLoginState.login)) {
       if (currentState == NsgLoginState.registration || !widget.widgetParams!.usePasswordLogin) {
         currentState = NsgLoginState.verification;
       } else {
@@ -355,7 +360,8 @@ class LoginWidgetState extends State<LoginWidget> {
   }
 
   ///Запросить код проверки в виде СМС или t-mail в зависимости от loginType
-  void doSmsRequest(BuildContext context, {NsgLoginType loginType = NsgLoginType.phone, String? password, required String firebaseToken}) {
+  void doSmsRequest(BuildContext context,
+      {NsgLoginType loginType = NsgLoginType.phone, String? password, required String firebaseToken}) {
     if (!_formKey.currentState!.validate()) return;
 
     NsgMetrica.reportLoginStart(loginType.toString());
@@ -371,7 +377,10 @@ class LoginWidgetState extends State<LoginWidget> {
       //Регистрация нового пользователя/восстановление пароля по e-mail или вход по паролю
       //Опраделяется наличием или отсутствием captchaCode
       widget.provider
-          .phoneLoginPassword(phoneNumber: loginType == NsgLoginType.phone ? phoneNumber : email, securityCode: captchaCode, loginType: loginType)
+          .phoneLoginPassword(
+              phoneNumber: loginType == NsgLoginType.phone ? phoneNumber : email,
+              securityCode: captchaCode,
+              loginType: loginType)
           .then((value) => checkRequestSMSanswer(context, value))
           .catchError((e) {
         widget.widgetParams!.showError(context, widget.widgetParams!.textCheckInternet);
@@ -379,7 +388,10 @@ class LoginWidgetState extends State<LoginWidget> {
     } else {
       widget.provider
           .phoneLoginRequestSMS(
-              phoneNumber: loginType == NsgLoginType.phone ? phoneNumber : email, securityCode: captchaCode, loginType: loginType, firebaseToken: firebaseToken)
+              phoneNumber: loginType == NsgLoginType.phone ? phoneNumber : email,
+              securityCode: captchaCode,
+              loginType: loginType,
+              firebaseToken: firebaseToken)
           .then((value) => checkRequestSMSanswer(context, value))
           .catchError((e) {
         widget.widgetParams!.showError(context, widget.widgetParams!.textCheckInternet);
@@ -442,15 +454,21 @@ class LoginWidgetState extends State<LoginWidget> {
           ),
           child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
-              child: Row(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center, children: [
-                Expanded(
-                    child: Column(mainAxisSize: MainAxisSize.min, crossAxisAlignment: CrossAxisAlignment.center, children: <Widget>[
-                  Text(
-                    widget.widgetParams!.textLoginSuccessful,
-                    style: widget.widgetParams!.headerMessageStyle,
-                  )
-                ]))
-              ]))),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                        child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                          Text(
+                            widget.widgetParams!.textLoginSuccessful,
+                            style: widget.widgetParams!.headerMessageStyle,
+                          )
+                        ]))
+                  ]))),
     );
   }
 
@@ -578,7 +596,10 @@ class LoginWidgetState extends State<LoginWidget> {
               },
               validator: (value) => value == null || value.length < 1 ? 'Password is required' : null),
         ),
-      if (kIsWeb || (!Platform.isAndroid && !Platform.isIOS)) widget.loginPage.getRememberMeCheckbox() else const SizedBox(height: 10),
+      if (kIsWeb || (!Platform.isAndroid && !Platform.isIOS))
+        widget.loginPage.getRememberMeCheckbox()
+      else
+        const SizedBox(height: 10),
       if (widget.widgetParams!.useCaptcha)
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -756,7 +777,8 @@ class LoginWidgetState extends State<LoginWidget> {
               ),
               initialValue: phoneNumber,
               onChanged: (value) => phoneNumber = value,
-              validator: (value) => isPhoneValid(value!) && value.length >= 9 ? null : widget.widgetParams!.textEnterCorrectPhone,
+              validator: (value) =>
+                  isPhoneValid(value!) && value.length >= 9 ? null : widget.widgetParams!.textEnterCorrectPhone,
             ),
           ),
       if (widget.widgetParams!.useEmailLogin)
@@ -816,6 +838,7 @@ class LoginWidgetState extends State<LoginWidget> {
       _getInput(
           hintText: widget.widgetParams!.textEnterCode,
           initialValue: securityCode,
+          autofillHints: [AutofillHints.oneTimeCode],
           keyboardType: TextInputType.number,
           onChanged: (value) => securityCode = value,
           validator: (value) => value == null || value.length < 6 ? 'Enter confirmation code from message' : null),
@@ -849,6 +872,7 @@ class LoginWidgetState extends State<LoginWidget> {
       {String hintText = '',
       String? initialValue,
       TextInputType keyboardType = TextInputType.text,
+      Iterable<String> autofillHints = const [],
       bool obscureText = false,
       Function(String)? onChanged,
       String? Function(String?)? validator}) {
@@ -856,8 +880,9 @@ class LoginWidgetState extends State<LoginWidget> {
       padding: const EdgeInsets.only(bottom: 10),
       child: TextFormField(
         key: GlobalKey(),
+        autofillHints: autofillHints,
         cursorColor: Theme.of(context).primaryColor,
-        keyboardType: TextInputType.emailAddress,
+        keyboardType: keyboardType,
         inputFormatters: null,
         style: TextStyle(color: nsgtheme.colorText),
         textAlign: TextAlign.center,
@@ -888,10 +913,15 @@ class LoginWidgetState extends State<LoginWidget> {
   ///securityCode - код верификации, полученный на предыдущем этапе
   ///loginType - тип логина (телефон/емаил)
   ///newPassword - новый (устанавливаемый) пароль
-  Future setNewPassword(BuildContext context, {required String securityCode, required NsgLoginType loginType, required String newPassword}) async {
+  Future setNewPassword(BuildContext context,
+      {required String securityCode, required NsgLoginType loginType, required String newPassword}) async {
     if (!_formKey.currentState!.validate()) return;
     widget.provider
-        .phoneLogin(phoneNumber: loginType == NsgLoginType.phone ? phoneNumber : email, securityCode: securityCode, register: true, newPassword: newPassword)
+        .phoneLogin(
+            phoneNumber: loginType == NsgLoginType.phone ? phoneNumber : email,
+            securityCode: securityCode,
+            register: true,
+            newPassword: newPassword)
         .then((value) => checkRequestNewPasswordanswer(context, value))
         .catchError((e) {
       widget.widgetParams!.showError(context, widget.widgetParams!.textCheckInternet);
